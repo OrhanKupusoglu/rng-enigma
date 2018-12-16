ab31681fd35981057a2d460b8ce2bf4a394d7399
Orhan Kupusoglu <orhan.kupusoglu@gmail.com>
2018-12-16 23:10:38 +0300

    Rename path args, to suppress SWIG warnings use pointer to array

--- library/include/rng_file.h
+++ library/include/rng_file.h
@@ -4,12 +4,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool rng_read_random(uint64_t rotors[16][16]);
 
-double rng_output_doubles_txt(const char* file_name, const long nums, bool rnd_sel);
+bool rng_read_random(const char* file_path, uint64_t* p_rotors);
 
-void rng_output_dieharder_bin(const char* file_name, const long nums);
+double rng_output_doubles_txt(const char* file_path, const long nums, bool rnd_sel);
 
-void rng_output_dieharder_txt(const char* file_name, const long nums);
+void rng_output_dieharder_bin(const char* file_path, const long nums);
+
+void rng_output_dieharder_txt(const char* file_path, const long nums);
 
 #endif /* _RNG_FILE_H */
